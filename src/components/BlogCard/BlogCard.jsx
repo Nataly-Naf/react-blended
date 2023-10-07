5c0514ce4b8d8b7395afe804c016f815d9dd6bad
Nataly-Naf <nat.spivak84@gmail.com>
2023-10-07 13:12:59 +0100

    1st task

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -14,6 +14,40 @@ import {
   Date,
 } from './BlogCard.styled';
 
-export const BlogCard = () => {
-  return <div>BlogCard</div>;
+import { formatDistanceToNow } from 'date-fns';
+
+export const BlogCard = ({
+  poster,
+  tag,
+  title,
+  description,
+  name,
+  avatar,
+  postedAt,
+}) => {
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardPoster src={poster} alt={title} />
+        </CardHeader>
+        <CardBody>
+          <Tag>{tag}</Tag>
+          <CardTitle>{title}</CardTitle>
+          <CardText>{description}</CardText>
+        </CardBody>
+        <CardFooter>
+          <UserBox>
+            <Avatar src={avatar} alt={name} />
+            <UserInfo>
+              <UserName>{name}</UserName>
+              <Date>
+                {formatDistanceToNow(new Date(postedAt), { addSuffix: true })}
+              </Date>
+            </UserInfo>
+          </UserBox>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
