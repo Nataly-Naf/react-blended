f95cc95eea17a6b7462083d11b6b10d188fcb18a
Pavel Nedoshyvko <plight83@gmail.com>
2023-11-04 15:24:46 +0200

    add homePage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { fetchByRegion } from 'service/country-service';
+
+export const Home = () => {
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchByRegion()
+      .then(res => setCountries(res))
+      .catch(error => setError(error))
+      .finally(setIsLoading(false));
+  }, []);
+
+  return (
+    <Section>
+      <Container>
+        {error && <Heading>{error.message}</Heading>}
+        {isLoading && <Heading>"LOADING..."</Heading>}
+        {countries.length > 0 && <CountryList countries={countries} />}
+      </Container>
+    </Section>
+  );
+};
